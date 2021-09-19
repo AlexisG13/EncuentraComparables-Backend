0d6a16ac64e63d5a02e43261d230a1a6ed303703
Alexis Gómez <00122717@uca.edu.sv>
2021-09-19 16:17:25 -0600

    ✨ Add puppeteer module

--- src/estate/estate.module.ts
+++ src/estate/estate.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { EstateService } from './estate.service';
+import { PuppeteerModule } from 'nest-puppeteer';
 
 @Module({
   providers: [EstateService],
+  imports: [PuppeteerModule.forRoot()],
 })
 export class EstateModule {}
--- src/estate/estate.service.ts
+++ src/estate/estate.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
+import { InjectBrowser } from 'nest-puppeteer';
+import { Browser } from 'puppeteer';
 
 @Injectable()
-export class EstateService {}
+export class EstateService {
+  constructor(@InjectBrowser() private readonly browser: Browser) {}
+}
