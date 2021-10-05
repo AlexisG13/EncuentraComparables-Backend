9023d728aa8c8422b713fe30495d50da291251dd
Alexis Gómez <00122717@uca.edu.sv>
2021-10-05 16:00:26 -0600

    ✨ Add estate controller

---
+++ src/estate/estate.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get, Query } from '@nestjs/common';
+import { EstateSearchFilters } from './dtos/get-states-query.dto';
+import { EstateService } from './estate.service';
+
+@Controller('estate')
+export class EstateController {
+  constructor(private readonly estateService: EstateService) {}
+
+  @Get()
+  getAllEstates(@Query() filters: EstateSearchFilters) {
+    return this.estateService.searchEstates(filters);
+  }
+}
