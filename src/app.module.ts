2a595a677adac1bd7c1788dcd4480b39926a73ec
Alexis Gómez <00122717@uca.edu.sv>
2021-10-05 15:59:31 -0600

    🔧 Add  config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { EstateModule } from './estate/estate.module';
 
 @Module({
-  imports: [EstateModule]
+  imports: [EstateModule, ConfigModule.forRoot({ isGlobal: true })],
 })
 export class AppModule {}
