8938fbe0d3f2e40d207af5b29affaecd70e87301
Alexis Gómez <00122717@uca.edu.sv>
2021-10-05 16:00:59 -0600

    ✨ Add estate services

--- src/estate/estate.service.ts
+++ src/estate/estate.service.ts
@@ -1,8 +1,42 @@
-import { Injectable } from '@nestjs/common';
-import { InjectBrowser } from 'nest-puppeteer';
+import { Inject, Injectable } from '@nestjs/common';
 import { Browser } from 'puppeteer';
+import { EstateSearchFilters } from './dtos/get-states-query.dto';
+import {
+  BienesRaicesDienca,
+  BienesRaicesEnElSalvador,
+  Encuentra24,
+  RemaxCentral,
+} from './estate_providers/encuentra24';
+import { EstateProvider } from './interfaces/estate-provider.interface';
+import { IEstate } from './interfaces/estate.interface';
 
 @Injectable()
 export class EstateService {
-  constructor(@InjectBrowser() private readonly browser: Browser) {}
+  constructor(@Inject('PUPPETEER_BROWSER') private browser: Browser) {}
+
+  private availableProviders = [
+    new Encuentra24(),
+    new BienesRaicesDienca(),
+    new RemaxCentral(),
+    new BienesRaicesEnElSalvador(),
+  ];
+
+  async getSingleProviderEstates(
+    provider: EstateProvider,
+    filters: EstateSearchFilters,
+  ): Promise<IEstate[]> {
+    const page = await this.browser.newPage();
+    await page.goto(provider.buildUrl(filters));
+    const content = await page.content();
+    await page.close();
+    return provider.parse(content);
+  }
+
+  async searchEstates(filters: EstateSearchFilters): Promise<IEstate[]> {
+    const estateRequests = Array.from(this.availableProviders).map((provider) =>
+      this.getSingleProviderEstates(provider, filters),
+    );
+    const estates = await Promise.all(estateRequests);
+    return estates.reduce((acum, curr) => acum.concat(curr));
+  }
 }
